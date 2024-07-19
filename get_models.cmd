965d4dd66feaefcf1072640d53f8b043c3d5f427
gbvolkov <gbvolkov@gmail.com>
2024-07-19 17:39:34 +0300

    1

--- get_models.cmd
+++ get_models.cmd
@@ -1,4 +1,5 @@
 git clone git@hf.co:pyannote/wespeaker-voxceleb-resnet34-LM
+ren wespeaker-voxceleb-resnet34-LM\pytorch_model.bin wespeaker-voxceleb-resnet34-LM\pyannote_pytorch_model.bin
 git clone git@hf.co:pyannote/segmentation-3.0
 git clone git@hf.co:pyannote/speaker-diarization-3.1
 git clone git@hf.co:jonatasgrosman/wav2vec2-large-xlsr-53-russian
