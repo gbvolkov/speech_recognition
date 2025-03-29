ec96a3a05553e94e505d247a06da06a8b0f4c598
GBV <gbvolkov@gmail.com>
2025-03-29 22:02:06 +0300

    editor small change

--- static/js/rename.js
+++ static/js/rename.js
@@ -1,15 +1,17 @@
-function applyRename(){
-    // Get the current HTML content from the Quill editor.
-    var htmlContent = quill.root.innerHTML;
-    var inputs = document.querySelectorAll('input[type="text"]');
-    inputs.forEach(function(input) {
-        var oldName = input.name;
-        var newName = input.value;
-        if(newName){
-            var regex = new RegExp(oldName, "g");
-            htmlContent = htmlContent.replace(regex, newName);
+function applyRename() {
+    let inputs = document.querySelectorAll('input[type="text"]');
+    inputs.forEach(input => {
+        let oldName = input.name;
+        let newName = input.value.trim();
+        if (newName) {
+            // Replace only text nodes containing oldName
+            document.querySelectorAll("#editor-container *").forEach(node => {
+                node.childNodes.forEach(child => {
+                    if (child.nodeType === Node.TEXT_NODE && child.nodeValue.includes(oldName)) {
+                        child.nodeValue = child.nodeValue.replaceAll(oldName, newName);
+                    }
+                });
+            });
         }
     });
-    // Update the Quill editor with the renamed content.
-    quill.root.innerHTML = htmlContent;
 }
